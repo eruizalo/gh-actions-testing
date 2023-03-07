6056442e332a446cae96e4ca443a1e40c60b769d
Eduardo Ruiz <eduardo.ruiz@hablapps.com>
2023-03-07 11:21:13 +0100

    add some TODOs

---
+++ src/main/scala/main.scala
@@ -0,0 +1,13 @@
+object Hello {
+  /**
+    * TODO comments
+    */
+}
+  def main(args: Array[String]): Unit = {
+    /*
+    TODO do something in main
+     */
+    println("Hello, world")
+    // todo end main
+  }
+}
